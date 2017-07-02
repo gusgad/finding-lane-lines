3796674317c53fab3e48156ee439ef69cf65b1e3
gusgad <gusgadirov@gmail.com>
2017-07-03 00:29:11 +0300

    Add Gaussian blur to image

--- image_detection.py
+++ image_detection.py
@@ -1,19 +1,14 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('tallinn_road_img.jpg')
+img = cv2.imread('assets/tallinn_road_img.jpg')
 
 def extract_white_and_yellow(img):
     # white color mask
-    lower = np.uint8([152, 152, 152])
-    upper = np.uint8([225, 225, 225])
-    white_mask = cv2.inRange(img, lower, upper)
-    # yellow color mask
-    lower = np.uint8([190, 190,   0])
-    upper = np.uint8([200, 200, 200])
-    yellow_mask = cv2.inRange(img, lower, upper)
+    lower = np.uint8([155, 155, 155])
+    upper = np.uint8([235, 235, 235])
+    mask = cv2.inRange(img, lower, upper)
     # combine the mask
-    mask = cv2.bitwise_or(white_mask, yellow_mask)
     masked = cv2.bitwise_and(img, img, mask = mask)
 
     return masked
@@ -21,10 +16,14 @@ def extract_white_and_yellow(img):
 def convert_gray_scale(img):
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
+def gaussian_blur(img, kernel_size=15):
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
 masked = extract_white_and_yellow(img)
 grayscaled = convert_gray_scale(masked)
+blurred = gaussian_blur(grayscaled)
 
-cv2.imshow('img', grayscaled)
+cv2.imshow('img', blurred)
 
 k = cv2.waitKey(0) & 0xFF
 if k == 27:
--- video_detection.py
+++ video_detection.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('tallinn_road_vid.mp4')
+cap = cv2.VideoCapture('assets/tallinn_road_vid.mp4')
 
 print(cap.isOpened())
 while True:
@@ -9,16 +9,9 @@ while True:
     if ret == True:
         def extract_white_and_yellow(img):
             # white color mask
-            lower = np.uint8([170, 170, 170])
-            upper = np.uint8([245, 245, 245])
-            white_mask = cv2.inRange(img, lower, upper)
-            # yellow color mask
-            lower = np.uint8([180, 180, 180])
-            upper = np.uint8([210, 210, 210])
-            yellow_mask = cv2.inRange(img, lower, upper)
-            # combine the mask
-            mask = cv2.bitwise_or(white_mask, yellow_mask)
-            masked = cv2.bitwise_and(img, img, mask = mask)
+            lower = np.uint8([155, 155, 155])
+            upper = np.uint8([240, 240, 240])
+            masked = cv2.inRange(img, lower, upper)
 
             return masked
 
